c81d5ffecdfd74ec5a2f951524b45a0cba3002a0
Johannes Brust <johannes.brust@dfki.de>
2024-09-10 15:40:07 +0200

    Switch to pathlib and GUI config file

--- chargepal_monitor_gui/config.py
+++ chargepal_monitor_gui/config.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python3
-import os
+from pathlib import Path
+import toml
 
-# Configuration and file paths
-server_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-envi_toml_file_path = os.path.join(server_dir, "chargepal_monitor_gui/cfg/envi.toml")
-databases_path = os.path.join(server_dir, "chargepal_local_server/src/chargepal_local_server/db")
-logs_path = os.path.join(server_dir, "chargepal_local_server/src/chargepal_local_server/logs")
-ldb_path = os.path.join(
-    databases_path,"ldb.db"
-)
-pdb_path = os.path.join(
-    databases_path,"pdb.db"
-)
-dfki_logo_path = os.path.join(server_dir, "chargepal_monitor_gui/img/dfki_logo.png")
-chargepal_logo_path = os.path.join(server_dir, "chargepal_monitor_gui/img/chargepal_logo.svg")
+monitor_gui_path = Path(__file__).parent
+
+# GUI configuration:
+gui_config_file_path = monitor_gui_path / "cfg/gui_config.toml"
+
+# Get chargepal_local_server directories:
+try:
+    with open(gui_config_file_path, "r") as f:
+        gui_config = toml.load(f)
+        server_dir = Path.home().joinpath(gui_config["server"]["server_dir"])
+except FileNotFoundError as e:
+    raise e
+logs_path = server_dir / "src/chargepal_local_server/logs"
+databases_path = server_dir / "src/chargepal_local_server/db"
+ldb_path = databases_path / "ldb.db"
+pdb_path = databases_path / "pdb.db"
+
+envi_toml_file_path = monitor_gui_path / "cfg/envi.toml"
+dfki_logo_path = monitor_gui_path / "img/dfki_logo.png"
+chargepal_logo_path = monitor_gui_path / "img/chargepal_logo.svg"
